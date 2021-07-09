6100d51971fd875235a7581204cfd148e910902e
Penny Lam <pennylam@live.com>
2021-07-09 10:44:20 +0900

    i first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const { DatabaseError } = require('pg');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => { res.send('its working') });
+app.post('/signin', signin.handleSignin (db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
+app.get('/profile/:id', profile.handleProfileGet(db));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res) })
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`its working on port  ${process.env.PORT}`);
+})
