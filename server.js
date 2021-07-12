3961507e65e6c7da33d06168f303527a5f0e98cf
Penny Lam <pennylam@live.com>
2021-07-12 17:07:25 +0900

    adding database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const { DatabaseError } = require('pg');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : '',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
 });
 
