e7f3d3566e2fa1be4ad1dccdffcaa1facb981562
Penny Lam <pennylam@live.com>
2021-07-14 17:33:55 +0900

    database fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const { DatabaseError } = require('pg');
 
 const db = knex({
     client: 'pg',
-      connectionString : process.env.DATABASE_URL,
+      connection : process.env.DATABASE_URL,
       ssl: true
 });
 
