8431a03fa344a0dfc0a00cf9f8f7816ea21ee563
Penny Lam <pennylam@live.com>
2021-07-14 16:03:11 +0900

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,7 @@ const handleRegister = (db, bcrypt) => (req, res) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'));
+    .catch(err => res.status(400).json(err));
 }
 
 module.exports = {
