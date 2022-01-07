a71379604ca181d289cb484a8686ae4bc4a679c7
Penny Lam <pennylam@live.com>
2022-01-07 16:30:42 +0900

    fix

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
- const db = knex({
+const db = knex({
     client: 'pg',
     connection: {
-    host : '127.0.0.1',
-    user : '',
-    password : '',
-    database : 'smart-brain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
- });
+  });
 
 const app = express();
 
