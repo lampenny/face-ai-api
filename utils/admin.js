a962e8b7518a11e9886135df11ee91cb8777934d
Penny Lam <pennylam@live.com>
2022-01-07 15:00:53 +0900

    fix

--- utils/admin.js
+++ utils/admin.js
@@ -2,42 +2,42 @@ const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const knex = require("knex");
 
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
 
-let pool;
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  pool = new Pool({
-    host: "127.0.0.1",
-    user: "Penny",
-    password: "",
-    port: process.env.PORT,
-    database: "smart-brain",
-  });
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      required: true,
-      rejectUnauthorized: false,
-    },
-    sslmode: "require",
-  });
-}
+// let pool;
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//   pool = new Pool({
+//     host: "127.0.0.1",
+//     user: "Penny",
+//     password: "",
+//     port: process.env.PORT,
+//     database: "smart-brain",
+//   });
+// } else {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       required: true,
+//       rejectUnauthorized: false,
+//     },
+//     sslmode: "require",
+//   });
+// }
 
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     user : '',
-//     password : '',
-//     database : 'smart-brain'
-//   }
-// });
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 //  DEVELOPMENT ONLY - check database
 // db.select('*').from('users').then(data => {
 //   console.log(data);
 // })
 
-module.exports = { pool, knex, bcrypt, express };
+module.exports = { db, knex, bcrypt, express };
 //add pool
