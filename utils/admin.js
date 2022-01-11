8b91561b6db0da13b2c49fdd80780fe2874d1359
Penny Lam <pennylam@live.com>
2022-01-11 16:25:44 +0900

    fix connection string

--- utils/admin.js
+++ utils/admin.js
@@ -5,9 +5,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL
-    },
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     }
