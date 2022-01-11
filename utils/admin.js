e2064c612fd65ad14ea67bd05951cce9ac269be8
Penny Lam <pennylam@live.com>
2022-01-11 15:57:30 +0900

    fix

--- utils/admin.js
+++ utils/admin.js
@@ -2,14 +2,11 @@ const bcrypt = require("bcrypt-nodejs");
 const knex = require("knex");
 
 const db = knex({
-    client: 'pg',
+  client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      host : '127.0.0.1',
-      // user : '',
-      // password : '',
-      database : 'smart-brain'
+      ssl: true
     }
-  });
+});
 
 module.exports = { knex, bcrypt, db };
