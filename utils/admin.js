2f60998f98fb601ed61d103143162eed82366a94
Penny Lam <pennylam@live.com>
2022-01-14 11:35:56 +0900

    update

--- utils/admin.js
+++ utils/admin.js
@@ -2,31 +2,31 @@ const express = require('express');
 const bcrypt = require("bcrypt-nodejs");
 const knex = require("knex");
 
-const db = knex({
-  client: 'pg',
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    }
-});
-
-// const Pool = require("pg").Pool;
-
-// let pool;
-// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-//   pool = new Pool({
-//     host: "127.0.0.1",
-//     database: "smart-brain",
-//   });
-// } else {
-//   pool = new Pool({
+// const db = knex({
+//   client: 'pg',
 //     connectionString: process.env.DATABASE_URL,
 //     ssl: {
-//       required: true,
 //       rejectUnauthorized: false,
-//     },
-//     sslmode: require,
-//   });
-// }
+//     }
+// });
+
+const Pool = require("pg").Pool;
+
+let pool;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  pool = new Pool({
+    host: "127.0.0.1",
+    database: "smart-brain",
+  });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      required: true,
+      rejectUnauthorized: false,
+    },
+    sslmode: require,
+  });
+}
 
 module.exports = { express, knex, bcrypt };
