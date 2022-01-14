031b9e1fbb0b9dc91f7e5cd87dc699a9ae66a06a
Penny Lam <pennylam@live.com>
2022-01-14 11:33:28 +0900

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { knex, bcrypt, db, pool } = require("./utils/admin");
+const { knex, bcrypt, db } = require("./utils/admin");
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -10,16 +10,17 @@ const image = require('./controllers/image');
 const cors = require('cors');
 const app = require("express")();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("it is working!");
 });
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
-app.post('/register', register.handleRegister(db, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', profile.handleProfileGet(db));
+//update image increments
 app.put('/image', image.handleImage(db));
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
